e2930f83628e79dd320524a74bf7229ff3dee7da
Papyrus219 <Papyrus219@o2.pl>
2026-06-23 17:04:27 +0200

    Base of pipeline abstraction

---
+++ include/libs/eruptor/Eruptor/lib/platform/pipeline.hpp
@@ -0,0 +1,17 @@
+#ifndef ERUPTOR_PLATFORM_PIPELINE_HPP
+#define ERUPTOR_PLATFORM_PIPELINE_HPP
+
+namespace eruptor::platform
+{
+
+class Pipeline
+{
+public:
+    void Init();
+private:
+    void Create_graphics_pipeline();
+};
+
+}
+
+#endif // ERUPTOR_PLATFORM_PIPELINE_HPP
---
+++ src/eruptor/platform/pipeline.cpp
@@ -0,0 +1,10 @@
+#include <Eruptor/lib/platform/pipeline.hpp>
+
+void eruptor::platform::Pipeline::Init()
+{
+
+}
+
+void eruptor::platform::Pipeline::Create_graphics_pipeline()
+{
+}
